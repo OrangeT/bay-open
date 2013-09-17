4161db3075c5713d15cd0df6cda77f2dc5004ca1
Kian Ryan <kian@orangetentacle.co.uk>
2013-09-17 11:17:39 +0100

    Moved background image loading in to script.

--- js/background.js
+++ js/background.js
@@ -1,36 +1,27 @@
-var photo_url = "";
-var photo_link = "";
-var photo_title = "";
+$(document).ready(function() {
+    var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=646ef8b09f982c21ecc46c5c976cb8ad&text=morecambe+bay&license=1%2C2%2C4%2C5%2C7&per_page=100&page=1&format=json&jsoncallback=?";
 
-var jsonFlickrApi = function(rsp) {
-    var index = Math.floor(Math.random() * 100) + 1;
+    $.getJSON(url, null, function(data) {
+         var index = Math.floor(Math.random() * 100) + 1;
 
-    if (rsp.stat == "ok" && rsp.photos.photo.length > 0) {
-        var photo = rsp.photos.photo[index];
+        if (data.stat == "ok" && data.photos.photo.length > 0) {
+            var photo = data.photos.photo[index];
 
-        // http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-        photo_url = "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "b.jpg";
+            // http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+            var photo_url = "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "b.jpg";
 
-        // http://www.flickr.com/photos/{user-id}/{photo-id}
-        photo_link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
+            // http://www.flickr.com/photos/{user-id}/{photo-id}
+            var photo_link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
 
-        photo_title = photo.title;
+            var photo_title = photo.title;
 
-        update();
-    }
-};
+            var css = 'url(' + photo_url + ')';
+            $("body").css("background-image", css);
 
-var update = function() {
-    if (document.readyState !== "complete") {
-        window.setTimeout(update, 500);
-        return;
-    }
-
-    var css = 'url(' + photo_url + ')';
-    $("body").css("background-image", css);
-
-    var photocredit = $('a.photocredit');
-    photocredit.attr("href", photo_link);
-    photocredit.text(photo_title);
-};
-
+            var photocredit = $('a.photocredit');
+            photocredit.attr("href", photo_link);
+            photocredit.text(photo_title);
+        }
+   
+    });
+});
