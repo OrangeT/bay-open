4d3e1b2609d641512d4a2bfe515a95cff6d0c68b
Kian Ryan <kian@orangetentacle.co.uk>
2014-12-23 15:23:18 +0000

    Flickr API must now be done over HTTPS

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=646ef8b09f982c21ecc46c5c976cb8ad&text=morecambe+bay&license=1%2C2%2C4%2C5%2C7&per_page=100&page=1&format=json&jsoncallback=?";
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=646ef8b09f982c21ecc46c5c976cb8ad&text=morecambe+bay&license=1%2C2%2C4%2C5%2C7&per_page=100&page=1&format=json&jsoncallback=?";
 
     $.getJSON(url, null, function(data) {
          var index = Math.floor(Math.random() * 100) + 1;
