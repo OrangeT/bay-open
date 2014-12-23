3fff31d6cb16c8345fee5ec7269b817f4657a401
Kian Ryan <kian@orangetentacle.co.uk>
2014-12-23 16:15:55 +0000

    Annual changes to bay open site

--- js/tickets.js
+++ js/tickets.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var url = "http://json2jsonp.com/?callback=?&url=https%3A%2F%2Fwww.eventbrite.com%2Fjson%2Fevent_list_attendees%3Fid%3D8361348017%26app_key%3DTK33PTRBQQXDJZQTEA";
+    var url = "http://json2jsonp.com/?callback=?&url=https%3A%2F%2Fwww.eventbrite.com%2Fjson%2Fevent_list_attendees%3Fid%3D15046250741%26app_key%3DTK33PTRBQQXDJZQTEA%26user_key%3D12529159772113316988";
 
     $.getJSON(url, null, function(data) {
 
@@ -10,9 +10,9 @@ $(document).ready(function() {
         //Epee: 20729031, 20729037
         //Foil: 20729033, 20729039
 
-        displayEntries(data.attendees, 20729029, 20729035, template, "#sabreEntry");
-        displayEntries(data.attendees, 20729031, 20729037, template, "#epeeEntry");
-        displayEntries(data.attendees, 20729033, 20729039, template, "#foilEntry");
+        displayEntries(data.attendees, 32320191, 32320194, template, "#sabreEntry");
+        displayEntries(data.attendees, 32320192, 32320195, "#epeeEntry");
+        displayEntries(data.attendees, 32320193, 32320196, template, "#foilEntry");
 
     });
 });
